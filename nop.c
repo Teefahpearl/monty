bb26ad8cf0d987149ce7612ff04548a1637daff5
Teefah_Pearl <lateefatalao787@gmail.com>
2024-01-18 19:14:39 -0800

    add

--- nop.c
+++ nop.c
@@ -1,12 +1,14 @@
 #include "monty.h"
+
 /**
-  *f_nop- nothing
-  *@head: stack head
-  *@counter: line_number
-  *Return: no return
- */
+*f_nop- nothing
+*@head: stack head
+*@counter: line_number
+*Return: nothing!
+*/
 void f_nop(stack_t **head, unsigned int counter)
 {
-	(void) counter;
-	(void) head;
+		(void) counter;
+
+		(void) head;
 }
